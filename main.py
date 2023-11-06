3afdb8c83895c12f5ce9f3590b3d3d7a47aabcaf
miomao34 <flex384@gmail.com>
2023-11-06 22:31:49 +0300

    added ded's msgs

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from os import getenv
+from os.path import splitext
 import logging
+from pathlib import Path
+from random import choice
 
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
+    # MessageHandler,
     ContextTypes,
 )
 
@@ -22,13 +26,24 @@ file_log_handler.setFormatter(formatter)
 logger.addHandler(stderr_log_handler)
 logger.addHandler(file_log_handler)
 
-logger.info("booting up")
+logger.info("booting up: loading main.py")
+
+
+TELEGRAM_MSG_LENGTH = 4096
+
+
+def pick_file(directory: str) -> str:
+    """returns a random chosen filename from a folder"""
+    filenames = list(Path(directory).rglob("*.*"))
+
+    return choice(filenames).name
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = [
         "a one-percenter-helper.",
-        "ask me about the pies!",
+        "ask me about the pies",
+        "or the gramp's fries",
     ]
 
     await context.bot.send_message(
@@ -37,7 +52,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
     logger.debug(
-        f"start: {update.effective_chat.id} - {update.effective_chat.full_name}"
+        f"start: {update.effective_chat.id} - {update.effective_chat.effective_name}"
     )
 
 
@@ -46,20 +61,57 @@ async def pie(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.send_photo(chat_id=update.effective_chat.id, photo=file)
 
     logger.debug(
-        f"got pied in {update.effective_chat.id} - {update.effective_chat.full_name}"
+        f"got pied in {update.effective_chat.id} - {update.effective_chat.effective_name}"
     )
 
 
+async def ded(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.debug(
+        f"got ded'ed in {update.effective_chat.id} - {update.effective_chat.effective_name}"
+    )
+
+    directory = "assets/ded/"
+    filename = pick_file(directory=directory)
+    full_path = directory + filename
+
+    logger.debug(f"ded's file {filename} chosen")
+
+    if splitext(filename)[1] == ".txt":
+        with open(full_path, "r") as file:
+            contents = file.read()
+            for i in range(0, len(contents), TELEGRAM_MSG_LENGTH):
+                await context.bot.send_message(
+                    chat_id=update.effective_chat.id,
+                    text=contents[i : i + TELEGRAM_MSG_LENGTH],
+                )
+            logger.debug("ded's copypasta sent!")
+    else:
+        with open(full_path, "rb") as file:
+            try:
+                await context.bot.send_photo(
+                    chat_id=update.effective_chat.id,
+                    photo=file,
+                )
+                logger.debug("screenshot of ded's msg sent!")
+            except Exception as e:
+                logger.error(f"tried to send photo {filename} but failed; not a photo?")
+                logger.error(e)
+
+
 if __name__ == "__main__":
+    logger.debug("starting up...")
     TG_TOKEN = getenv("TG_TOKEN")
     if not TG_TOKEN:
         logger.fatal("no TG_TOKEN!")
         raise ValueError("no TG_TOKEN!")
+    logger.debug("TG_TOKEN loaded")
 
     app = ApplicationBuilder().token(TG_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("pie", pie))
+    app.add_handler(CommandHandler("ded", ded))
+    # app.add_handler(MessageHandler())
 
     logger.info("starting polling...")
     app.run_polling()
